10aa038a409c2b7d778c5c3f0b81ac7786cc6dd1
Shervin <khanim97@gmail.com>
2021-05-07 01:20:46 +0430

    commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Heroku Hosting
+Read [Related Article](https://juniorfrontend.ir/%D9%87%D8%A7%D8%B3%D8%AA-%D8%B1%D8%A7%DB%8C%DA%AF%D8%A7%D9%86-node-js-%D8%B1%D8%A7-%D8%A8%D8%A7-heroku-%D8%AA%D8%AC%D8%B1%D8%A8%D9%87-%DA%A9%D9%86%DB%8C%D8%AF/)!
+
+## Run
+```
+yarn install
+yarn start
+```
---
+++ app.js
@@ -0,0 +1,25 @@
+// ---------------------------------------------
+//                   IMPORTS
+// ---------------------------------------------
+const express = require('express')();  
+
+// ---------------------------------------------
+//          app.get
+// ---------------------------------------------
+app.get('/', function (req, res) {
+
+  // send
+  res.send(`
+    <div>
+      <h1 style="color: lightgray">Hello Heroku! ;)</h1>
+      <h3 style="color:red">Yoohoo!!!</h3>
+      <p style="color: darkblue">This is my first app in Heroku!</p>
+    </div>
+  `)
+
+});
+
+// ---------------------------------------------
+//                server 
+// ---------------------------------------------
+app.listen(process.env.PORT || 5000);
